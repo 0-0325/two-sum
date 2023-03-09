1286f97ffe2a67be0bfef73f29e12ca58cbb466b
0-0325 <ryan160060@gmail.com>
2023-03-09 21:40:07 +0900

    initial version of hello.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+def twoSum(nums, target):
+    Returnlist=[]
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if (nums[i]+nums[j])==target:
+                Returnlist.append(i)
+                Returnlist.append(j)
+                return Returnlist
+
+
+
+
